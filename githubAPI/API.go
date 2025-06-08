a37149e6245c8e87ede11faf9845dd9f2e3158bc
Aidan Wendorf <96208746+aidanjinn@users.noreply.github.com>
2025-06-08 16:20:49 -0500

    Json Loading and Tests

--- githubAPI/API.go
+++ githubAPI/API.go
@@ -6,16 +6,63 @@ import (
 	"net/http"
 )
 
-type Response struct {
-	Data []byte
+type GithubUser struct {
+	UserActions []Action
 }
 
-func Ping(username string) (int, Response, error) {
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", "https://api.github.com/users/aidanjinn/events/public?per_page=1", nil)
+type Action struct {
+	Type    string  `json:"type"`
+	Actor   Actor   `json:"actor"`
+	Repo    Repo    `json:"repo"`
+	Payload Payload `json:"payload"`
+}
+
+type Actor struct {
+	DisplayName string `json:"display_login"`
+	Url         string `json:"url"`
+}
+
+type Repo struct {
+	RepoName string `json:"name"`
+	Url      string `json:"url"`
+}
+
+type Payload struct {
+	Commits []Commits `json:"commits"`
+}
+
+type Commits struct {
+	Author  Author `json:"author"`
+	Message string `json:"message"`
+}
+
+type Author struct {
+	Email string `json:"email"`
+	Name  string `json:"name"`
+}
+
+func LoadJSON(resp *http.Response) ([]Action, error) {
+	body, err := io.ReadAll(resp.Body)
 
 	if err != nil {
-		return 0, Response{}, err
+		return []Action{}, err
+	}
+
+	var data []Action
+	if err := json.Unmarshal(body, &data); err != nil {
+		return []Action{}, err
+	}
+
+	return data, nil
+}
+
+func Ping(username string) (int, error) {
+	client := &http.Client{}
+	url := "https://api.github.com/users/" + username + "/events/public?per_page=1"
+	req, err := http.NewRequest("GET", url, nil)
+
+	if err != nil {
+		return 0, err
 	}
 
 	req.Header.Set("Accept", "application/vnd.github+json")
@@ -24,20 +71,10 @@ func Ping(username string) (int, Response, error) {
 	resp, err := client.Do(req)
 
 	if err != nil {
-		return 0, Response{}, err
+		return 0, err
 	}
 	// This will close the resp once the ping function ends
 	defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
-
-	if err != nil {
-		return 0, Response{}, err
-	}
-
-	var jsonResp interface{}
-	err = json.Unmarshal(body, &jsonResp)
-	retResp := Response{Data: body}
-
-	return resp.StatusCode, retResp, nil
+	return resp.StatusCode, nil
 }
--- tests/CLI_test.go
+++ tests/CLI_test.go
@@ -2,7 +2,9 @@ package tests
 
 import (
 	"githubCLI/githubAPI"
+	"io"
 	"net/http"
+	"strings"
 	"testing"
 )
 
@@ -12,18 +14,91 @@ func errOut(t *testing.T, err error) {
 	}
 }
 
+func TestDriver(t *testing.T) {
+	TestPing(t)
+	TestJSON(t)
+}
+
 // Pinging the Github API and receiving a proper response
 // Function Name we are testing is ping
 func TestPing(t *testing.T) {
-	statusCode, resp, err := githubAPI.Ping("aidanjinn")
+
+	// This should return a proper response for my github account activity
+	statusCode, err := githubAPI.Ping("aidanjinn")
 
 	errOut(t, err)
 
+	// Failure for not getting a response at all
 	if statusCode != http.StatusOK {
 		t.Errorf("Expected status code %d, got %d", http.StatusOK, statusCode)
 	}
+}
 
-	if len(resp.Data) == 0 {
-		t.Errorf("Expected a response but recived an empty response buffer")
+func TestJSON(t *testing.T) {
+	jsonData := `[{
+		"id": "50586368742",
+		"type": "PushEvent",
+		"actor": {
+			"id": 96208746,
+			"login": "aidanjinn",
+			"display_login": "aidanjinn",
+			"gravatar_id": "",
+			"url": "https://api.github.com/users/aidanjinn",
+			"avatar_url": "https://avatars.githubusercontent.com/u/96208746?"
+		},
+		"repo": {
+			"id": 438780222,
+			"name": "aidanjinn/aidanjinn",
+			"url": "https://api.github.com/repos/aidanjinn/aidanjinn"
+		},
+		"payload": {
+			"repository_id": 438780222,
+			"push_id": 24735860700,
+			"size": 1,
+			"distinct_size": 1,
+			"ref": "refs/heads/main",
+			"head": "6401fba434db35583b566910f7373100fce823ac",
+			"before": "6c212deafbaea9a94c29f62bbc83a858b0e4267c",
+			"commits": [
+			{
+				"sha": "6401fba434db35583b566910f7373100fce823ac",
+				"author": {
+				"email": "96208746+aidanjinn@users.noreply.github.com",
+				"name": "Aidan Wendorf"
+			},
+			"message": "Update README.md",
+			"distinct": true,
+			"url": "https://api.github.com/repos/aidanjinn/aidanjinn/commits/6401fba434db35583b566910f7373100fce823ac"
+		}]},
+		"public": true,
+		"created_at": "2025-06-06T05:47:49Z"
+	}]`
+	resp := &http.Response{
+		Body:       io.NopCloser(strings.NewReader(jsonData)),
+		StatusCode: http.StatusOK,
+	}
+
+	actions, err := githubAPI.LoadJSON(resp)
+	errOut(t, err)
+
+	if len(actions) != 1 {
+		t.Fatalf("Expected 1 action, got %d", len(actions))
+	}
+
+	action := actions[0]
+	if action.Type != "PushEvent" {
+		t.Errorf("Expected action type 'PushEvent', got '%s'", action.Type)
+	}
+	if action.Actor.DisplayName != "aidanjinn" {
+		t.Errorf("Expected actor 'aidanjinn', got '%s'", action.Actor.DisplayName)
+	}
+	if action.Repo.RepoName != "aidanjinn/aidanjinn" {
+		t.Errorf("Expected repo name 'aidanjinn/test-repo', got '%s'", action.Repo.RepoName)
+	}
+	if action.Payload.Commits[0].Author.Name != "Aidan Wendorf" {
+		t.Errorf("Expected author name 'aidanjinn', got '%s'", action.Payload.Commits[0].Author.Name)
+	}
+	if action.Payload.Commits[0].Message != "Update README.md" {
+		t.Errorf("Expected message 'Update README.md', got '%s'", action.Payload.Commits[0].Message)
 	}
 }
